01b0eb3158c19d10710c7564e908acf5ec4093c9
Bea <beanilsson1@gmail.com>
2017-08-25 12:55:04 +0200

    Fix syntax error

--- lumos_rpg/items.py
+++ lumos_rpg/items.py
@@ -20,7 +20,7 @@ class Gold(Item):
 class Weapon(Item):
     def __init__(self, name, description, value, damage):
         self.damage = damage
-        super.__init__(name, description, value)
+        super().__init__(name, description, value)
 
     def __str__(self):
         return "{}\n====\n{}\nDamage: {}\n".format(self.name, self.description, self.value, self.damage)
