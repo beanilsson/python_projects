c79d053a6f3ae4f7e606dec3ade50ec056eeba46
Bea <beanilsson1@gmail.com>
2017-09-08 12:22:48 +0200

    Print the matrix

--- image_recognition/imagerec.py
+++ image_recognition/imagerec.py
@@ -2,7 +2,8 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-i = Image.open('images/dot.png')
+i = Image.open('images/numbers/y0.5.png')
 image_array = np.asarray(i)
 plt.imshow(image_array)
+print image_array
 plt.show()
