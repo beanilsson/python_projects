fe02de81b6eb99c6522e137eb471c2db61f49ee8
Bea <beanilsson1@gmail.com>
2017-09-08 09:17:47 +0000

    WIP: Open image

--- image_recognition/imagerec.py
+++ image_recognition/imagerec.py
@@ -1,7 +1,12 @@
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
-i = Image.open('images/dot.png')
+i = Image.open('images/dotndot.png')
 image_array = np.asarray(i)
-
-print image_array
+plt.imshow(image_array)
+plt.show()
