72d56c104bc8869f853f5b5ef0c962fb8b03a0d8
Bea <beanilsson1@gmail.com>
2017-09-08 13:48:47 +0200

    Create examples

--- image_recognition/thresholding.py
+++ image_recognition/thresholding.py
@@ -3,6 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+def createExamples():
+    numberArrayExamples = open('numArEx.txt', 'a')
+    numbersWeHave = range(0,10)
+    versionsWeHave = range(1,10)
+
+    for eachNum in numbersWeHave:
+        for eachVer in versionsWeHave:
+            #print str(eachNum) + '.' + str(eachVer)
+            imgFilePath = 'images/numbers/' + str(eachNum) + '.' + str(eachVer) + '.png'
+            exampleImage = Image.open(imgFilePath)
+            exampleImageArray = np.array(exampleImage)
+            exampleImageArray1 = str(exampleImageArray.tolist())
+
+            lineToWrite = str(eachNum) + '::' + exampleImageArray1 + '\n'
+            numberArrayExamples.write(lineToWrite)
+
+
+createExamples()
+
+
 def threshold(imageArray):
     balanceArray = []
     newArray = imageArray
@@ -42,19 +62,19 @@ i3Array = np.array(i3)
 i4 = Image.open('images/sentdex.png')
 i4Array = np.array(i4)
 
-threshold(i2Array)
-threshold(i3Array)
-threshold(i4Array)
+# threshold(i2Array)
+# threshold(i3Array)
+# threshold(i4Array)
 
-fig = plt.figure()
-ax1 = plt.subplot2grid((8,6), (0,0), rowspan=4, colspan=3)
-ax2 = plt.subplot2grid((8,6), (4,0), rowspan=4, colspan=3)
-ax3 = plt.subplot2grid((8,6), (0,3), rowspan=4, colspan=3)
-ax4 = plt.subplot2grid((8,6), (4,3), rowspan=4, colspan=3)
+# fig = plt.figure()
+# ax1 = plt.subplot2grid((8,6), (0,0), rowspan=4, colspan=3)
+# ax2 = plt.subplot2grid((8,6), (4,0), rowspan=4, colspan=3)
+# ax3 = plt.subplot2grid((8,6), (0,3), rowspan=4, colspan=3)
+# ax4 = plt.subplot2grid((8,6), (4,3), rowspan=4, colspan=3)
 
-ax1.imshow(i1Array)
-ax2.imshow(i2Array)
-ax3.imshow(i3Array)
-ax4.imshow(i4Array)
+# ax1.imshow(i1Array)
+# ax2.imshow(i2Array)
+# ax3.imshow(i3Array)
+# ax4.imshow(i4Array)
 
-plt.show()
+# plt.show()
