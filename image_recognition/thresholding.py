3a59a8d013ffc9281bdeb6df27644f91c2d98cae
Bea <beanilsson1@gmail.com>
2017-09-08 13:24:30 +0200

    Thresholding

---
+++ image_recognition/thresholding.py
@@ -0,0 +1,60 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+def threshold(imageArray):
+    balanceArray = []
+    newArray = imageArray
+
+    for eachRow in imageArray:
+        for eachPixel in eachRow:
+            avgNum = reduce(lambda x, y: x + y, eachPixel[:3])/len(eachPixel[:3])
+            balanceArray.append(avgNum)
+    balance = reduce(lambda x, y: x + y, balanceArray)/len(balanceArray)
+
+    for eachRow in newArray:
+        for eachPixel in eachRow:
+            if reduce(lambda x, y: x + y, eachPixel[:3])/len(eachPixel[:3]) > balance:
+                eachPixel[0] = 255
+                eachPixel[1] = 255
+                eachPixel[2] = 255
+                eachPixel[3] = 255
+            else:
+                eachPixel[0] = 0
+                eachPixel[1] = 0
+                eachPixel[2] = 0
+                eachPixel[3] = 255
+
+    return newArray
+
+
+
+i1 = Image.open('images/numbers/0.1.png')
+i1Array = np.array(i1)
+
+i2 = Image.open('images/numbers/y0.4.png')
+i2Array = np.array(i2)
+
+i3 = Image.open('images/numbers/y0.5.png')
+i3Array = np.array(i3)
+
+i4 = Image.open('images/sentdex.png')
+i4Array = np.array(i4)
+
+threshold(i2Array)
+threshold(i3Array)
+threshold(i4Array)
+
+fig = plt.figure()
+ax1 = plt.subplot2grid((8,6), (0,0), rowspan=4, colspan=3)
+ax2 = plt.subplot2grid((8,6), (4,0), rowspan=4, colspan=3)
+ax3 = plt.subplot2grid((8,6), (0,3), rowspan=4, colspan=3)
+ax4 = plt.subplot2grid((8,6), (4,3), rowspan=4, colspan=3)
+
+ax1.imshow(i1Array)
+ax2.imshow(i2Array)
+ax3.imshow(i3Array)
+ax4.imshow(i4Array)
+
+plt.show()
