def39209bd36701a8e36ac41f4dff801f3dec987
Bea <beanilsson1@gmail.com>
2017-09-08 12:12:15 +0200

    Remove extra import

--- image_recognition/imagerec.py
+++ image_recognition/imagerec.py
@@ -1,12 +1,8 @@
-import matplotlib
-# Force matplotlib to not use any Xwindows backend.
-matplotlib.use('Agg')
-
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-i = Image.open('images/dotndot.png')
+i = Image.open('images/dot.png')
 image_array = np.asarray(i)
 plt.imshow(image_array)
 plt.show()
