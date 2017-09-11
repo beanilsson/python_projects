5d6520b6b4a0c4f0b21ec4697c7265a5e0da0e62
Bea <beanilsson1@gmail.com>
2017-09-11 14:23:50 +0200

    Add basic logic

--- shopping_list/app.py
+++ shopping_list/app.py
@@ -1,1 +1,19 @@
-#### Shopping list
+def shopping_list(items):
+    shopping_list = items
+    print('Items to purchase: ')
+    for i in items:
+        print(i)
+
+def get_item():
+    item = ''
+    list = []
+    while True:
+        item = raw_input('Enter an item, or DONE to quit ')
+        if (item == 'DONE'):
+            break
+        else:
+            list.append(item)
+
+    shopping_list(list)
+
+get_item()
