e66bad32ef075b3fad5097fdf93360464066d0ce
Bea <beanilsson1@gmail.com>
2017-09-11 14:55:19 +0200

    Add more logic

--- shopping_list/app.py
+++ shopping_list/app.py
@@ -1,16 +1,21 @@
 def shopping_list(items):
-    shopping_list = items
     print('Items to purchase: ')
     for i in items:
         print(i)
 
 def get_item():
-    item = ''
     list = []
+    item = ''
     while True:
-        item = raw_input('Enter an item, or DONE to quit ')
+        item = input('Enter an item, or type DONE to quit: ')
         if (item == 'DONE'):
             break
+        elif (item == 'SHOW'):
+            print(shopping_list(list))
+            continue
+        elif (item == 'HELP'):
+            print('Type DONE to quit. Type SHOW to show the contents of the list. Type HELP to display this message.')
+            continue
         else:
             list.append(item)
 
